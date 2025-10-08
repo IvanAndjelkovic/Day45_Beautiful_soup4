317522b521ad31ca4a90280adf19fcb8386c1ad5
Ivan Andjelkovic <83224969+IvanAndjelkovic@users.noreply.github.com>
2025-10-08 10:02:41 +0000

    Once more opening the file

--- Parsing_HTML_making_soup/main.py
+++ Parsing_HTML_making_soup/main.py
@@ -2,7 +2,7 @@
 
 
 
-with open("website.html","r", encoding = "utf-8") as f:
-    content  = f.read()
+with open("/workspaces/Day45_Beautiful_soup4/Parsing_HTML_making_soup/website.html", "r", encoding="utf-8") as f:
+    content = f.read()
 
 print(content)
