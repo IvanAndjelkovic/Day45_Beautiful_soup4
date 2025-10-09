b4cc7cd6a1c38bbed910005b88a8ba1156004264
Ivan Andjelkovic <83224969+IvanAndjelkovic@users.noreply.github.com>
2025-10-09 18:59:00 +0000

    practicing selecting with CSS selectors using BeautifulSoup

--- Parsing_HTML_making_soup/main.py
+++ Parsing_HTML_making_soup/main.py
@@ -1,8 +1,33 @@
-
+from bs4 import BeautifulSoup
 
 
 
 with open("/workspaces/Day45_Beautiful_soup4/Parsing_HTML_making_soup/website.html", "r", encoding="utf-8") as f:
     content = f.read()
 
-print(content)
+soup = BeautifulSoup(content, "html.parser")
+# print(soup.prettify()  )
+
+all_anchor_tags = soup.find_all(name = "a" )
+
+# for tag in all_anchor_tags:
+#     # text = tag.getText()
+#     link = tag.get("href")
+#     print(link)
+
+# heading = soup.find(name="h1", id="name").getText()
+# print(heading)
+
+# section_heading = soup.find(name="h3", class_ = "heading")
+# print(section_heading.getText())
+
+# company_url = soup.select_one(selector = "p a")
+# print(company_url)
+
+# select ID 
+name = soup.select_one(selector = "#name")
+print(name)
+
+heading = soup.select(selector =".heading")
+print(heading)
+
