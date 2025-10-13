c9774d71edf1051e21121923d5de634675a59aa1
Ivan Andjelkovic <83224969+IvanAndjelkovic@users.noreply.github.com>
2025-10-13 19:41:25 +0000

    Searching for a most voted article on Hacker News

--- Parsing_HTML_making_soup/main.py
+++ Parsing_HTML_making_soup/main.py
@@ -5,10 +5,30 @@ response = requests.get("https://news.ycombinator.com/news")
 yc_web_page = response.text
 
 soup = BeautifulSoup(yc_web_page, 'html.parser')
+
+
 articles = soup.find_all(class_="titleline")
-for  article in articles:
-    text = soup.find_all('a')
-    print(text)
+article_texts = []
+article_links = []
+article_upvotes = []
+for article in articles:
+    article_tag = article.find("a")
+    article_text = article_tag.getText()
+    article_link = article_tag.get("href")
+    article_upvote = article.find_next(class_="score").getText() 
+    article_texts.append(article_text)
+    article_links.append(article_link)
+    article_upvotes.append(int(article_upvote.split()[0]))
+
+print(article_texts)
+print(article_links)
+print(article_upvotes)
+largest_number = max(article_upvotes)
+largest_index = article_upvotes.index(largest_number)
+print(article_texts[largest_index])
+print(article_links[largest_index])
+print(article_upvotes[largest_index])
+
 
 
 
